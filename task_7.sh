bc940e329f0a1197927158c1bca3f2f50ed6f73a
KseniiaKarpova <31700970+KseniiaKarpova@users.noreply.github.com>
2019-11-16 12:46:27 +0300

    Update task_7.sh

--- task_7.sh
+++ task_7.sh
@@ -4,21 +4,20 @@ print() {
 for f in $1/*
 do
 
-if [[ -d "$f" ]]
+if [[ -d "$f" ]] #проверяет папка это или нет
 then
-print $f
+print $f #вызывается рекурсия
 else
-echo "Размер файла:";
-echo -n $(wc -c $f | awk '{print $1}');
-echo " bytes";
-echo "Название файла:";
-wc -c $f | awk '{print $2}'; 
-echo "Права доступа:";
-ls -l $f | awk '{print $1}';
-echo "------------";
+echo "Размер файла:"
+echo -n $(wc -c $f | awk '{print $1}'); # не пустая строка; размер файла в байтах ( вывод лишь размера, без дополнительной инфы)
+echo " bytes"
+echo "Название файла:"
+echo "$f"; 
+echo "Права доступа:"
+ls -l $f | awk '{print $1}'
+echo "------------"
 echo
 fi
 done
 }
 print $folder
-
